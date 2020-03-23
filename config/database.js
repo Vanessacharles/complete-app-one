cbe24472540b99b3fd5643fc49401482653e0bea
Vanessa Charles <vanessacharles@Vanessas-MacBook-Air.local>
2020-03-23 08:45:50 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://demo:first@cluster0-ohlzf.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'avenger'
+};
